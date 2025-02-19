6e4bc20b78268589700854d277d863c2d4ad86b6
Binita rijal <rijalbinita01@gmail.com>
2025-02-19 21:28:34 +0545

    ejs installed

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ app.get("/",(req,res)=>{
     res.send("homepage has startted");
     })
     app.get("/bini",(req,res)=>{
-        res.json({
-            message:"hello baby baby",
-            name:"pretty"
-        })
+        // res.json({
+        //     message:"hello baby baby",
+        //     name:"pretty"
+        // })
+
+        res.render("home.ejs")
         })
     app.get("/contact",(req,res)=>{
-        res.json({
-            message:"i am contact"
-            
-        })
+       res.send("hellooo")
     })
 
     app.listen(3000,(req,res)=>{
